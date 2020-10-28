f857131ef004c133bfa58e8b0eef5a984eb290dc
Riccardo Bianchi <riccardobianchi@Riccardos-MacBook-Pro.local>
2020-10-28 16:22:35 +0100

    Test pipeline

--- App/main.py
+++ App/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from the test app!"
+    return "Hello from the test app! This is updated content!!"
 
 
 if __name__ == "__main__":
