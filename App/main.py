11bf846648ded64ab193c4d63e2ac3968d7738e4
Riccardo Bianchi <riccardobianchi@Riccardos-MacBook-Pro.local>
2020-10-28 17:30:51 +0100

    Changed text content in Flask App

--- App/main.py
+++ App/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from the test app! This is updated content!!"
+    return "Hello from the test app!"
 
 
 if __name__ == "__main__":
